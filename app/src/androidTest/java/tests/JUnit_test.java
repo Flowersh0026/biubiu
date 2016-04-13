9a46632b3d425007a73543ec888d40d954b38a2e
Flowersh0026 <wangyunjia1996@gmail.com>
2016-04-13 11:26:05 -0700

    ..

---
+++ app/src/androidTest/java/tests/JUnit_test.java
@@ -0,0 +1,29 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.yuw248.bilibili.MainActivity;
+import com.example.yuw248.bilibili.R;
+
+/**
+ * Created by yuw248 on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+    public JUnit_test() {
+
+        super(MainActivity.class);
+    }
+
+    public void test_first() {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World !",tester);
+    }
+
+}
